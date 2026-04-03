60d64fb3eb6b1ffcfc9a5518ede9a565cf71495d
Emily_Chu <yc4747@columbia.edu>
2026-04-03 13:42:21 -0400

    Update bigquery_helpers authentication

--- bigquery_helpers.py
+++ bigquery_helpers.py
@@ -3,25 +3,18 @@ from __future__ import annotations
 import pandas as pd
 import streamlit as st
 from google.cloud import bigquery
+from google.oauth2 import service_account
 
 
 @st.cache_resource
 def get_bigquery_client() -> bigquery.Client:
-    credentials_info = {
-        "type": st.secrets["gcp_service_account"]["type"],
-        "project_id": st.secrets["gcp_service_account"]["project_id"],
-        "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
-        "private_key": st.secrets["gcp_service_account"]["private_key"],
-        "client_email": st.secrets["gcp_service_account"]["client_email"],
-        "client_id": st.secrets["gcp_service_account"]["client_id"],
-        "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
-        "token_uri": st.secrets["gcp_service_account"]["token_uri"],
-        "auth_provider_x509_cert_url": st.secrets["gcp_service_account"][
-            "auth_provider_x509_cert_url"
-        ],
-        "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"],
-    }
-    return bigquery.Client.from_service_account_info(credentials_info)
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"]
+    )
+    return bigquery.Client(
+        credentials=credentials,
+        project=credentials.project_id,
+    )
 
 
 def run_query(query: str) -> pd.DataFrame:
